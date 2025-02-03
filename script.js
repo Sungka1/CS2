080101e3653527ff3f89c3572ae6abf6ab7190fb
Sungka1 <lawrencelat101@gmail.com>
2025-02-03 18:31:18 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const messages = [
+    "Are you sure?",
+    "Really sure??",
+    "Are you positive?",
+    "please...",
+    "Just think about it..",
+    "If you say no, I will be really sad😔...",
+    "I will be very sad🥹...",
+    "I will be very very very sad😭😿...",
+    "Ok fine, I will stop asking...",
+    "Just kidding, say yes please!",
+    "Just click yes 👆🏻",
+    "it's blocking your screen😱"
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+    const noButton = document.querySelector('.no-button');
+    const yesButton = document.querySelector('.yes-button');
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function handleYesClick() {
+    window.location.href = "yes_page.html";
+}
